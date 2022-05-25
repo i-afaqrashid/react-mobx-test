44e2459cb3de93752c6db0353a8c714574f1940b
i-afaqrashid <i.afaqrashid@gmail.com>
2022-05-25 06:26:20 -0700

    initial commit from react app

---
+++ webpack.config.ts
@@ -0,0 +1,46 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import path from 'path';
+import { Configuration } from 'webpack';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+const config: Configuration = {
+  entry: './src/index.tsx',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  // @ts-ignore
+  devServer: {
+    static: path.join(__dirname, 'build'),
+    compress: true,
+    port: 4000,
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({
+      async: false,
+    }),
+  ],
+};
+
+export default config;
